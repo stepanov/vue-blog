0e5530123674180727c9fb08b1f18caab212a141
Michael Stepanov <michael@stepanoff.org>
2019-04-13 21:20:43 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div id="app">
+    <header>
+      <h1>Vue.js SPA</h1>
+    </header>
+    <main>
+      <aside class="sidebar">
+        <div v-for="post in posts">
+          res: {{ post.title }}
+        </div>
+      </aside>
+      <div class="content">
+      </div>
+    </main>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+  export default {
+    data () {
+      return {
+        posts: null,
+        endpoint: 'https://jsonplaceholder.typicode.com/posts/'
+      }
+    },
+    created() {
+      this.getAllPosts();
+    },
+    methods: {
+      getAllPosts() {
+        axios.get(this.endpoint)
+          .then(response => {
+            this.posts = response.data
+          })
+          .catch(error => {
+            console.log("-------------error-----------");
+            console.log(error);
+          })
+      }
+    }
+  }
+</script>
